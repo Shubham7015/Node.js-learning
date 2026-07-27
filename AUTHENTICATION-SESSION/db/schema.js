20707e3863da40e50c4ae92dad34fe20e98bf29b
Shubham Rohilla <srohilla099@gmail.com>
2026-07-27 18:25:52 +0530

    feat(schema): add user role enum and role column to users table
    - Define pgEnum 'user_role' with values USER and ADMIN
    - Add 'role' column to usersTable with default 'USER'
    - Remove unused 'integer' and 'time' imports

--- AUTHENTICATION-SESSION/db/schema.js
+++ AUTHENTICATION-SESSION/db/schema.js
@@ -1,10 +1,13 @@
-import { integer, pgTable, varchar, uuid, text , timestamp } from "drizzle-orm/pg-core";
-import { time } from "node:console";
+
+import { pgTable, varchar, uuid, text , timestamp ,pgEnum } from "drizzle-orm/pg-core";
+
+export const userRoleEnum = pgEnum('user_role',['USER','ADMIN']) ;
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  role: userRoleEnum().notNull().default('USER'),
   password: text().notNull(),
   salt: text().notNull(),
 });
